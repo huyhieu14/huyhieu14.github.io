1f87b9615c614ae9ef4682ea2e84436dbae0fb32
huyhieu14 <83973708+huyhieu14@users.noreply.github.com>
2021-06-09 00:59:43 +0700

    upload

---
+++ script/dist/nav-vertical.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var tabsVerticalInner = $('#accordian');
+var selectorVerticalInner = $('#accordian').find('li').length;
+var activeItemVerticalInner = tabsVerticalInner.find('.active');
+var activeWidthVerticalHeight = activeItemVerticalInner.innerHeight();
+var activeWidthVerticalWidth = activeItemVerticalInner.innerWidth();
+var itemPosVerticalTop = activeItemVerticalInner.position();
+var itemPosVerticalLeft = activeItemVerticalInner.position();
---
+++ script/nav-vertical.js
@@ -0,0 +1,7 @@
+var tabsVerticalInner = $('#accordian');
+var selectorVerticalInner = $('#accordian').find('li').length;
+var activeItemVerticalInner = tabsVerticalInner.find('.active');
+var activeWidthVerticalHeight = activeItemVerticalInner.innerHeight();
+var activeWidthVerticalWidth = activeItemVerticalInner.innerWidth();
+var itemPosVerticalTop = activeItemVerticalInner.position();
+var itemPosVerticalLeft = activeItemVerticalInner.position();
