d8bd82bf2ca3ea664e3df7514f584e83fa8f01ff
huyhieu14 <83973708+huyhieu14@users.noreply.github.com>
2021-06-09 23:55:16 +0700

    upload

---
+++ script/dist/header.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+$('.input-prev').click(function () {
+  console.log("Da click"); // var result = document.getElementById('quantity');
+  // var qty = result.value;
+  // if (!isNaN(qty) && qty > 1) result.value--;
+  // return false;
+});
+console.log(document.getElementsByClassName(" input-prev")[0]);
---
+++ script/header.js
@@ -0,0 +1,9 @@
+$('.input-prev').click(() => {
+    console.log("Da click")
+        // var result = document.getElementById('quantity');
+        // var qty = result.value;
+        // if (!isNaN(qty) && qty > 1) result.value--;
+        // return false;
+})
+
+console.log(document.getElementsByClassName(" input-prev")[0]);
