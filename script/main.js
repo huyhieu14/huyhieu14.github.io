a5705d6584e73b015b818a0a2e3b0591d878e8db
huyhieu14 <83973708+huyhieu14@users.noreply.github.com>
2021-06-12 04:04:47 +0700

    upload

--- script/main.js
+++ script/main.js
@@ -1,3 +1,9 @@
-$('#better-rating-form').betterRating({
-    wrapper:'#better-rating-list'
+$(".js-range-slider").ionRangeSlider({
+    type: "double",
+    grid: true,
+    min: 1000000,
+    max: 1000000000,
+    from: 200,
+    to: 800,
+    prefix: "VNĐ "
 });
